fb6fd40a9a8bc79f8dbe9a14c017e2df992ac891
danielamorimp <daniel_amorimpacheco@hotmail.com>
2018-10-21 17:35:22 -0300

    questoes das vogais

--- App.js
+++ App.js
@@ -1,30 +1,57 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, 
+        StyleSheet, 
+        Text, 
+        View,
+        TextInput,
+        TouchableOpacity,
+        ImageBackground} from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
 
-type Props = {};
-export default class App extends Component<Props> {
+
+export default class App extends Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      bg: "F",
+      bg2: "U",
+      bg3: "A",
+      bg4: "L"
+    }
+  }
+
+ 
+
   render() {
+    
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Luiz Miguel Teste</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <ImageBackground source={require('./app/imagens/tela_perguntas.png')} style={styles.container}>
+        <View style={styles.espacoPergunta}>
+            <Text style={styles.pergunta}>Identifique quais letras abaixo são vogais: </Text>
+        </View>
+
+        <View style = {styles.resposta}>
+          <TouchableOpacity style = {styles.options} onPress = {() => this.setState({bg: "F-Errou"})}>
+            <Text style = {styles.optiontext}>{this.state.bg}</Text>
+          </TouchableOpacity >
+          
+          <TouchableOpacity style = {styles.options} onPress = {() => this.setState({bg2:"U-Acertou"})}>
+            <Text style = {styles.optiontext}>{this.state.bg2}</Text>
+          </TouchableOpacity>
+        </View>
+
+        <View style = {styles.resposta}>  
+          <TouchableOpacity style = {styles.options} onPress = {() => this.setState({bg3:"A-Acertou"})}>
+            <Text style = {styles.optiontext}>{this.state.bg3}</Text>
+          </TouchableOpacity>
+          
+          <TouchableOpacity style = {styles.options} onPress = {() => this.setState({bg4:"L-Errou"})}>
+            <Text style = {styles.optiontext}>{this.state.bg4}</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground>
     );
   }
 }
@@ -32,18 +59,55 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+
+resposta:{
+  flexDirection:"row",
+  justifyContent:"space-around",
+},
+
+pergunta:{
+  alignSelf: "center",
+  padding: 30,
+  fontSize: 20,
+  fontWeight: "bold",
+  color: "black"
+},
+
+espacoPergunta: {
+  backgroundColor: "white",
+  width: "80%",
+  alignSelf: "center",
+  marginTop: 10,
+  borderRadius: 10,
+  borderWidth: 1.5,
+  borderColor: "black"
+},
+
+options:{
+  alignSelf: "center",
+  borderRadius: 10,
+  borderWidth: 1.5,
+  borderColor: "black",
+  marginTop: 15,
+},
+
+optiontext:{
+  fontSize: 20,
+  borderRadius: 10,
+  fontWeight: "bold",
+  color: "black",
+  backgroundColor: "white",
+  padding: 25,
+},
+
+optiontext2:{
+  fontSize: 20,
+  borderRadius: 10,
+  fontWeight: "bold",
+  color: "black",
+  backgroundColor: "red",
+  padding: 25,
+},
+
 });
