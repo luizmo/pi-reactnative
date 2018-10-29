042a6e21ef5727712a64799fe1646adfef500bc5
brunoacruzeiro <brunoacruzeiro@unipam.edu.br>
2018-10-29 01:05:56 -0300

    Update final dos audios

--- components/questao10.js
+++ components/questao10.js
@@ -3,26 +3,45 @@ import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, I
 import { StackNavigator } from 'react-navigation';
 import questao11 from '.';
 
+var SoundPlayer = require('react-native-sound');
+var song = null;
+
 class questao10 extends Component{
   static navigationOptions = {
     title:'questao10',
   }
   constructor(props){
     super(props)
-    this.state = { numero10:this.props.navigation.state.params.numero9 }
+    this.state = { numero10:this.props.navigation.state.params.numero9 };
+    this.state ={ pause: false, };
   }
+
+  componentWillMount(){
+    song = new SoundPlayer('questaoconta.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+  }
+
+  onPressButtonPlay() {
+    if (song != null) {
+      song.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <ImageBackground source={require('../img/bg_secu.png')} style={[styles.container,{width:"100%", height:"100%"}]}>
-        <View style={styles.pergunta}>
-          <Text style={styles.texto}>
-            Toque na resposta certa.
-          </Text>
-          <View>
-            <Image source={require('../img/atividades-6-7/atividade10/p4.jpg')} />
+      <ImageBackground source={require('../img/bg_secu.png')} style={[styles.container,{width:"100%", height:"100%"}]} onLoad={this.onPressButtonPlay.bind(this)}>
+        <TouchableOpacity style={styles.play} onPress={this.onPressButtonPlay.bind(this)}>
+          <Image  style={styles.player} source={require('../img/player.png')} />
+        </TouchableOpacity>
+          <View  style={styles.conta}>
+            <Image source={require('../img/atividades-6-7/atividade10/p4.jpg')} style={styles.contaimg}/>
           </View>
-        </View>
         <View style={styles.alternativas}>
           <TouchableOpacity style={styles.icones} onPress={()=> navigate('questao11', {numero10:this.state.numero10 + 0})}>
             <Image  style={styles.imagem} source={require('../img/atividades-6-7/atividade10/mm.jpg')} />
@@ -53,17 +72,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
-  pergunta: {
-    marginBottom: "5%",
-    alignItems:"center",
-    backgroundColor:"white",
-    borderRadius: 16,
-    marginTop: "2%",
-    marginRight: "8%",
-    marginLeft: "8%",
-    borderWidth:4,
-    borderColor:"purple",
-  },
   texto: {
     fontSize: 35,
     textAlign: "center",
@@ -87,5 +95,23 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-  }
+  },
+  player: {
+    width:"100%",
+    height:"100%"
+  },
+  play: {
+    width:"39%",
+    height:"39%",
+    flex: 1,
+    flexDirection: "row",
+    marginLeft:"28%"
+   },
+   conta: {
+    width:"40%",
+    marginLeft:"16%",
+   },
+   contaimg: {
+    borderRadius: 16,
+   }
 });
--- components/questao5.js
+++ components/questao5.js
@@ -3,23 +3,42 @@ import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, I
 import { StackNavigator } from 'react-navigation';
 import questao6 from '.';
 
+var SoundPlayer = require('react-native-sound');
+var song = null;
+
 class questao5 extends Component{
   static navigationOptions = {
     title:'questao5',
   }
   constructor(props){
     super(props)
-    this.state = { numero5:this.props.navigation.state.params.numero4 }
+    this.state = { numero5:this.props.navigation.state.params.numero4 };
+    this.state ={ pause: false, };
   }
+
+  componentWillMount(){
+    song = new SoundPlayer('questao5.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+  }
+
+  onPressButtonPlay() {
+    if (song != null) {
+      song.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <ImageBackground source={require('../img/bg_secu.png')} style={[styles.container,{width:"100%", height:"100%"}]}>
-        <View style={styles.pergunta}>
-          <Text style={styles.texto}>
-            Toque no objeto que não fica na cozinha.
-          </Text>
-        </View>
+      <ImageBackground source={require('../img/bg_secu.png')} style={[styles.container,{width:"100%", height:"100%"}]} onLoad={this.onPressButtonPlay.bind(this)}>
+        <TouchableOpacity style={styles.play} onPress={this.onPressButtonPlay.bind(this)}>
+          <Image  style={styles.player} source={require('../img/player.png')} />
+        </TouchableOpacity>
         <View style={styles.alternativas}>
             <TouchableOpacity style={styles.icones} onPress={()=> navigate('questao6', {numero5:this.state.numero5 + 0})}>
               <Image  style={styles.imagem} source={require('../img/atividades-6-7/atividade5/r.jpg')} />
@@ -65,7 +84,7 @@ const styles = StyleSheet.create({
     color:'purple',
   },
   icones: {
-    width: "48%",
+    width: "46%",
     height: "75%",
     borderStyle:"solid",
   },
@@ -82,5 +101,16 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-  }
+  },
+  player: {
+    width:"100%",
+    height:"100%"
+  },
+  play: {
+    width:"39%",
+    height:"39%",
+    flex: 1,
+    flexDirection: "row",
+    marginLeft:"28%"
+   }
 });
