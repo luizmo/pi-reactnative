31b046845e1c0fc5e27b6d42344a0cd55a3b490b
danielamorimp <daniel_amorimpacheco@hotmail.com>
2018-11-29 14:19:01 -0200

    audio questao 55

--- components/questao55.js
+++ components/questao55.js
@@ -3,13 +3,66 @@ import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, I
 import { StackNavigator } from 'react-navigation';
 import questao56 from '.';
 
+var SoundPlayer = require('react-native-sound');
+var song3 = null;
+var song4 = null;
+var song5 = null;
+
+
 class questao55 extends Component{
-    static navigationOptions = {
-      title:'questao55',
+  static navigationOptions = {
+    title:'questao55',
+  }
+  constructor(props){
+    super(props)
+    this.state = { pause: false }
+  }
+
+  componentWillMount(){
+
+    song3 = new SoundPlayer('erro3.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song4 = new SoundPlayer('erro4.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song5 = new SoundPlayer('erro5.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+  }
+
+  erro1() {
+    if (song3 != null) {
+      song3.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
     }
-    constructor(props){
-      super(props)
+  }
+
+  erro2() {
+    if (song4 != null) {
+      song4.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
     }
+  }
+
+  erro3() {
+    if (song5 != null) {
+      song5.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
 
   render() {
     const { navigate } = this.props.navigation;
@@ -22,14 +75,14 @@ class questao55 extends Component{
           <Image style={styles.img_questao} source={require('../img/atividades-8-9/coliseu.jpeg')} />
         </View>
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro3.bind(this)} >
             <View style={styles.imagem}>
               <Text style={styles.opcoes}>Torre Eiffel</Text>
 
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro1.bind(this)} >
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >Cristo Redentor</Text>
             </View>
@@ -38,7 +91,7 @@ class questao55 extends Component{
         </View>
 
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro2.bind(this)} >
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >Panteão</Text>
             </View>
