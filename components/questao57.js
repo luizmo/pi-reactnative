808b1f11218d0811ee4437a8f71d05dde8de053f
LMiguelOliveira <luizmiguel.oliveira18@gamil.com>
2018-11-29 14:35:36 -0200

    audio questao 57

--- components/questao57.js
+++ components/questao57.js
@@ -3,12 +3,64 @@ import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, I
 import { StackNavigator } from 'react-navigation';
 import questao58 from '.';
 
+var SoundPlayer = require('react-native-sound');
+var song3 = null;
+var song4 = null;
+var song5 = null;
+
+
 class questao57 extends Component{
   static navigationOptions = {
     title:'questao57',
   }
   constructor(props){
     super(props)
+    this.state = { pause: false }
+  }
+
+  componentWillMount(){
+
+    song3 = new SoundPlayer('erro3.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song4 = new SoundPlayer('erro4.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song5 = new SoundPlayer('erro5.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+  }
+
+  erro1() {
+    if (song3 != null) {
+      song3.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
+  erro2() {
+    if (song4 != null) {
+      song4.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
+  erro3() {
+    if (song5 != null) {
+      song5.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
   }
 
   render() {
@@ -21,7 +73,7 @@ class questao57 extends Component{
           </Text>
         </View>
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones}>
+          <TouchableOpacity style={styles.icones}onPress={this.erro1.bind(this)}>
             <Image  style={styles.imagem} source={require('../img/atividades-8-9/rio.jpg')} />
           </TouchableOpacity>
 
@@ -31,11 +83,11 @@ class questao57 extends Component{
         </View>
 
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro2.bind(this)}>
             <Image style={styles.imagem} source={require('../img/atividades-8-9/bahia.png')} />
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro3.bind(this)}>
             <Image style={styles.imagem} source={require('../img/atividades-8-9/ceara.png')} />
           </TouchableOpacity>
         </View>
