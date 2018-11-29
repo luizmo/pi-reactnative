134b51669f6ce42f6d05feb19d80a088dac42725
LMiguelOliveira <luizmiguel.oliveira18@gamil.com>
2018-11-29 14:46:18 -0200

    audio questao 59

--- components/questao59.js
+++ components/questao59.js
@@ -4,14 +4,65 @@ import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, I
 import { StackNavigator } from 'react-navigation';
 import questao60 from '.';
 
+var SoundPlayer = require('react-native-sound');
+var song3 = null;
+var song4 = null;
+var song5 = null;
+
+
 class questao59 extends Component{
   static navigationOptions = {
     title:'questao59',
   }
   constructor(props){
     super(props)
+    this.state = { pause: false }
   }
 
+  componentWillMount(){
+
+    song3 = new SoundPlayer('erro3.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song4 = new SoundPlayer('erro4.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song5 = new SoundPlayer('erro5.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+  }
+
+  erro1() {
+    if (song3 != null) {
+      song3.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
+  erro2() {
+    if (song4 != null) {
+      song4.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
+  erro3() {
+    if (song5 != null) {
+      song5.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -29,7 +80,7 @@ class questao59 extends Component{
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones}onPress={this.erro3.bind(this)} >
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >...A B C D</Text>
             </View>
@@ -38,13 +89,13 @@ class questao59 extends Component{
         </View>
 
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones}  >
+          <TouchableOpacity style={styles.icones}onPress={this.erro2.bind(this)}  >
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >...R S T U</Text>
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones}>
+          <TouchableOpacity style={styles.icones}onPress={this.erro3.bind(this)}>
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >...M N O P</Text>
 
