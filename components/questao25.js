e53052c4d73ee9b7cd9bf5feed3ba03ac33d6aab
Gabriel110 <gabrielcorreia@unipam.edu.br>
2018-11-29 14:31:04 -0200

    Gabriel e o melhor em React

--- components/questao25.js
+++ components/questao25.js
@@ -5,6 +5,9 @@ import questao26 from '.';
 
 var SoundPlayer = require('react-native-sound');
 var song = null;
+var song3 = null;
+var song4 = null;
+var song5 = null;
 
 class questao25 extends Component{
   static navigationOptions = {
@@ -20,6 +23,20 @@ class questao25 extends Component{
       if (error)
         ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
     });
+    song3 = new SoundPlayer('erro3.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song4 = new SoundPlayer('erro4.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song5 = new SoundPlayer('erro5.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
   }
 
   onPressButtonPlay() {
@@ -30,6 +47,33 @@ class questao25 extends Component{
       });
     }
   }
+  erro1() {
+    if (song3 != null) {
+      song3.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT);
+      });
+    }
+  }
+
+  erro2() {
+    if (song4 != null) {
+      song4.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT);
+      });
+    }
+  }
+
+  erro3() {
+    if (song5 != null) {
+      song5.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT);
+      });
+    }
+  }
+
   render() {
     const { navigate } = this.props.navigation;
     return (
@@ -38,21 +82,21 @@ class questao25 extends Component{
           <Image  style={styles.player} source={require('../img/player.png')} />
         </TouchableOpacity>
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro2.bind(this)}>
             <Image  style={styles.imagem} source={require('../img/atividades-3-4/cama.png')} />
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro3.bind(this)}>
             <Image style={styles.imagem} source={require('../img/atividades-3-4/pa.png')} />
           </TouchableOpacity>
         </View>
 
-        <View style={styles.alternativas}>
+        <View style={styles.alternativas} >
           <TouchableOpacity style={styles.icones} onPress={()=> navigate('questao26')}>
             <Image style={styles.imagem} source={require('../img/atividades-3-4/talher.png')} />
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones}>
+          <TouchableOpacity style={styles.icones} onPress={this.erro2.bind(this)}>
             <Image style={styles.imagem} source={require('../img/atividades-3-4/pedra.png')} />
           </TouchableOpacity>
         </View>
