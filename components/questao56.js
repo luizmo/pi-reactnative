5cbac8e4ebc3981e9924fde203f3b501168259b7
LMiguelOliveira <luizmiguel.oliveira18@gamil.com>
2018-11-29 14:28:51 -0200

    audio questao 56

--- components/questao56.js
+++ components/questao56.js
@@ -3,13 +3,66 @@ import { StyleSheet, Text, View, TextInput, ImageBackground, TouchableOpacity, I
 import { StackNavigator } from 'react-navigation';
 import questao57 from '.';
 
+var SoundPlayer = require('react-native-sound');
+var song3 = null;
+var song4 = null;
+var song5 = null;
+
+
 class questao56 extends Component{
-    static navigationOptions = {
-      title:'questao56',
+  static navigationOptions = {
+    title:'questao56',
+  }
+  constructor(props){
+    super(props)
+    this.state = { pause: false }
+  }
+
+  componentWillMount(){
+
+    song3 = new SoundPlayer('erro3.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song4 = new SoundPlayer('erro4.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+
+    song5 = new SoundPlayer('erro5.mp3', SoundPlayer.MAIN_BUNDLE, (error) => {
+      if (error)
+        ToastAndroid.show('Error when init SoundPlayer :(((', ToastAndroid.SHORT);
+    });
+  }
+
+  erro1() {
+    if (song3 != null) {
+      song3.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
     }
-    constructor(props){
-      super(props)
+  }
+
+  erro2() {
+    if (song4 != null) {
+      song4.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
     }
+  }
+
+  erro3() {
+    if (song5 != null) {
+      song5.play((success) =>{
+        if(!success)
+        ToastAndroid.show('Error when play SoundPlayer :(((', ToastAndroid.SHORT); 
+      });
+    }
+  }
+
 
   render() {
     const { navigate } = this.props.navigation;
@@ -29,7 +82,7 @@ class questao56 extends Component{
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro3.bind(this)} >
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >Argentina</Text>
             </View>
@@ -38,13 +91,13 @@ class questao56 extends Component{
         </View>
 
         <View style={styles.alternativas}>
-          <TouchableOpacity style={styles.icones} >
+          <TouchableOpacity style={styles.icones} onPress={this.erro1.bind(this)} >
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >Uruguai</Text>
             </View>
           </TouchableOpacity>
 
-          <TouchableOpacity style={styles.icones}>
+          <TouchableOpacity style={styles.icones} onPress={this.erro2.bind(this)}>
             <View style={styles.imagem}>
               <Text style={styles.opcoes} >Brasil</Text>
 
